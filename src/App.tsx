d0eeddddd9867fed9fab50655d18fe511bfbe5f5
HollyCoder <137598096+HollyCoder00@users.noreply.github.com>
2025-08-28 05:47:58 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -228,8 +228,8 @@ function App() {
         </section>
         
         {/* Footer */}
-        <footer className="fixed bottom-0 right-0 z-10 text-['#fff']">
-            <p>&copy; 2024 TANZO SPACE DESIGN. {t('footer.copyright')}</p>
+        <footer className="fixed bottom-0 right-0 z-10">
+            <p className="text-white">&copy; 2024 TANZO SPACE DESIGN. {t('footer.copyright')}</p>
         </footer>
       </main>
     </div>
